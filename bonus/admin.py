ff748eaecbd949efcf0818e6436769d91bce6f86
nazijob <nazi32815@gmail.com>
2025-05-30 14:55:18 +0300

    add model and admin

--- bonus/admin.py
+++ bonus/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from bonus.models import *
+
+
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'created_at')
+    search_fields = ('name', 'phone')
+    ordering = ('-created_at',)
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'balance', 'created_at','used_at')
+    search_fields = ('id', 'user__name')
+    ordering = ('-used_at',)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'card', 'employee', 'amount', 'type', 'created_at')
+    search_fields = ('card__user__name','card__id', 'employee__name')
+    list_filter = ('type',)
+    ordering = ('-created_at',)
+
+
--- bonus/models.py
+++ bonus/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    phone = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Card(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cards")
+    balance = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    used_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Card {self.id} – User {self.user.name}'
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+class  Transaction(models.Model):
+    TYPE_CHOICES=  [
+        ('deposit',"зачисление"),
+        ("withdraw", "списание")
+    ]
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="transactions")
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="transactions")
+    amount = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} – {self.amount} руб на карту {self.card.amount}"
+
+
+
+
+
+
+
